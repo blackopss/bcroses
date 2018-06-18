b47e74e11dde66b74195d5d0e394d1723dd503df
blackopss <36933564+blackopss@users.noreply.github.com>
2018-06-19 00:46:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,7 +55,7 @@ client.on('message', message => {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "BC-Roese";
+    let copy = "Bc-HEKAYH | حــكـــايـــة.";
     let request = `Requested By ${message.author.username}`;
     if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
     msg.react('✅')
@@ -78,7 +78,7 @@ client.on('message', message => {
        .addField('Server', message.guild.name)
        .addField('Sender', message.author.username)
        .addField('Message', args)
-       .setImage("https://cdn.discordapp.com/attachments/413099021088063498/449691917052280863/image.jpg")
+       .setImage("https://cdn.discordapp.com/attachments/458161660117647360/458382331493744652/51f3ebbbede14373.jpg")
        .setThumbnail(message.author.avatarURL)
        .setFooter(copy, client.user.avatarURL);
     m.send({ embed: bc })
